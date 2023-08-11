1d6e6f26695c8a25eeb1f88ad41a54cf15166373
fnkbsk <igoico10@gmail.com>
2023-08-11 13:57:56 -0300

    commit

--- js/game.js
+++ js/game.js
@@ -20,3 +20,15 @@ const createElement = (tag, className) => {
     element.className = className;
     return element;
 }
+
+let firstCard = '';
+let secondCard = '';
+
+const checkEndGame = () => {
+    const disabledCards = document.querySelectorAll('.disabled-card');
+
+    if (disabledCards.length === 20) {
+        clearInterval(this.loop);
+        alert(`Parabéns, ${spanPlayer.innerHTML}! Si tiempo es de: ${timer.innerHTML}`);
+    }
+}
