642b97d710b641a4694b92d0b61eaa5120638cc5
fnkbsk <igoico10@gmail.com>
2023-08-11 13:52:10 -0300

    commit

---
+++ js/game.js
@@ -0,0 +1,22 @@
+const grid = document.querySelector('.grid');
+const spanPlayer = document.querySelector('.player');
+const timer = document.querySelector('.timer');
+
+const characters = [
+    'beth',
+    'jerry',
+    'jessica',
+    'morty',
+    'pessoa-passaro',
+    'pickle-rick',
+    'rick',
+    'summer',
+    'meeseeks',
+    'scroopy',
+];
+
+const createElement = (tag, className) => {
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
