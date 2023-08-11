307b75cefd687d22dbd4c46fd7baa534ade1397c
fnkbsk <igoico10@gmail.com>
2023-08-11 14:02:10 -0300

    commit

--- js/game.js
+++ js/game.js
@@ -1,34 +1,59 @@
-const grid = document.querySelector('.grid');
-const spanPlayer = document.querySelector('.player');
-const timer = document.querySelector('.timer');
+const grid = document.querySelector(".grid");
+const spanPlayer = document.querySelector(".player");
+const timer = document.querySelector(".timer");
 
 const characters = [
-    'beth',
-    'jerry',
-    'jessica',
-    'morty',
-    'pessoa-passaro',
-    'pickle-rick',
-    'rick',
-    'summer',
-    'meeseeks',
-    'scroopy',
+  "beth",
+  "jerry",
+  "jessica",
+  "morty",
+  "pessoa-passaro",
+  "pickle-rick",
+  "rick",
+  "summer",
+  "meeseeks",
+  "scroopy",
 ];
 
 const createElement = (tag, className) => {
-    const element = document.createElement(tag);
-    element.className = className;
-    return element;
-}
+  const element = document.createElement(tag);
+  element.className = className;
+  return element;
+};
 
-let firstCard = '';
-let secondCard = '';
+let firstCard = "";
+let secondCard = "";
 
 const checkEndGame = () => {
-    const disabledCards = document.querySelectorAll('.disabled-card');
+  const disabledCards = document.querySelectorAll(".disabled-card");
 
-    if (disabledCards.length === 20) {
-        clearInterval(this.loop);
-        alert(`Parabéns, ${spanPlayer.innerHTML}! Si tiempo es de: ${timer.innerHTML}`);
-    }
-}
+  if (disabledCards.length === 20) {
+    clearInterval(this.loop);
+    alert(
+      `Parabéns, ${spanPlayer.innerHTML}! Si tiempo es de: ${timer.innerHTML}`
+    );
+  }
+};
+
+const checkCards = () => {
+  const firstCharacter = firstCard.getAttribute("date-character");
+  const secondCharacter = secondCard.getAttribute("date-character");
+
+  if (firstCharacter === secondCharacter) {
+    firstCard.firstChild.classList.add("disabled-card");
+    secondCard.firstChild.classList.add("disabled-card");
+
+    firstCard = "";
+    secondCard = "";
+
+    checkEndGame();
+  } else {
+    setTimeout(() => {
+      firstCard.classList.remove("disabled-card");
+      secondCard.classList.remove("disabled-card");
+
+      firstCard = "";
+      secondCard = "";
+    }, 500);
+  }
+};
